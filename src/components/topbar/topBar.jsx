a8d47d1e02a614e85183e5deb33285eef4de2d37
1Mathias <mathiasabdisa@gmail.com>
2022-11-13 14:45:04 +0300

    top-bar update

--- src/components/topbar/topBar.jsx
+++ src/components/topbar/topBar.jsx
@@ -1,88 +1,104 @@
 // import { Link } from "react-router-dom";
 // import image from '../../image/logo.jpg';
+import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from "react-router-dom";
+import { Context } from '../../context/Context';
 import image from '../../image/logo.jpg';
 import "./topBar.css";
 
 
 export default function TopBar() {
-  const currentUser = false;
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:5000/images/";
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
   return (
-        <Navbar bg="light" expand='md' className="topbar">
-          <Container>
-            <Navbar.Brand href="#">
+    <Navbar bg="light" expand='md' className="topbar">
+      <Container>
+        <Navbar.Brand href="#">
+          <img className="header_icon" src={image} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-md`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
               <img className="header_icon" src={image} alt="logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-md`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
-                  <img className="header_icon" src={image} alt="logo" />
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">
-                    <Link className="link" to="/">
-                      Home
-                    </Link>
-                  </Nav.Link>
-                  {/* <Nav.Link href="#action2">Religion</Nav.Link> */}
-                  <NavDropdown title="Religion" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Orthodox</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action4">
-                    Islam
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Catholic
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action6">
-                    Jehovah
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action7">
-                    Only-Jesus
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action8">
-                    Adventist
-                  </NavDropdown.Item>
-                </NavDropdown>
-                  <Nav.Link href="#action9">
-                    <Link className="link" to="/write">
-                      Videos
-                    </Link>
-                  </Nav.Link>
-                  <Nav.Link href="#action10">Testemonies</Nav.Link>
-                  <Nav.Link href="#action11">Contact Us</Nav.Link>
-                  <Nav.Link href="#action12" className="login-btn">Login   |</Nav.Link>
-                  {/* <Nav.Link href="#">|</Nav.Link> */}
-                  <Nav.Link href="#action13">Register</Nav.Link>
-                  <Nav.Link>
-                    <div class='search-box'>
-                      <input class="search-text" type="text" placeholder="Search Anything" />
-                      <a href="#" class="search-btn">
-                        <i class="fas fa-search"></i>
-                      </a>
-                    </div>
-                  </Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">
+                <Link className="link" to="/">
+                  Home
+                </Link>
+              </Nav.Link>
+              <NavDropdown title="Religion" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Orthodox</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action4">
+                  Islam
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Catholic
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action6">
+                  Jehovah
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action7">
+                  Only-Jesus
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action8">
+                  Adventist
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link>
+                Videos
+              </Nav.Link>
+              <Nav.Link><Link className="link" to="/write">
+                Write
+              </Link></Nav.Link>
+              <Nav.Link ><Link className="link" to="/contactus">Contact Us</Link></Nav.Link>
+              <Nav.Link href="#action12" className="login-btn">
+                <Link className="link" to="/login">
+                  LOGIN
+                </Link>
+              </Nav.Link>
+              <Nav.Link><li className="topListItem">
+                <Link className="link" to="/register">
+                  REGISTER
+                </Link>
+              </li>
+              </Nav.Link>
+
+              <Nav.Link>
+                <div class='search-box'>
+                  <input class="search-text" type="text" placeholder="Search Anything" />
+                  <a href="#" class="search-btn">
+                    <i class="fas fa-search"></i>
+                  </a>
+                </div>
+              </Nav.Link>
+            </Nav>
+
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar >
   )
 }
 
